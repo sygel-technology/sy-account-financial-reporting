3ef374733ad616bd85499d00ebf9117a9588fe47
Roger <roger.sans@sygel.es>
2024-07-08 13:37:41 +0200

    [T-6312][ADD]hide_invoice_shipping_address: Keep the shipping address hidden on the invoice.

---
+++ hide_invoice_shipping_address/models/res_config_settings.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Roger Sans <roger.sans@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    invoice_shipping_address_print = fields.Boolean(
+        string='Print The Shipping Invoice On The Invoice',
+        related="company_id.invoice_shipping_address_print",
+        readonly=False,
+    )
