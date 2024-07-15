b8a01212420d86f736de28781cf596949e5d0a5c
Roger <roger.sans@sygel.es>
2024-07-15 16:06:43 +0200

    [IMP] hide_invoice_shipping_address: pre-commit auto fixes

---
+++ hide_invoice_shipping_address/models/res_partner.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Roger Sans <roger.sans@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    invoice_shipping_address_print = fields.Selection(
+        string='Print The Shipping Invoice On The Invoice',
+        selection=[
+            ('show', 'Show'),
+            ('hide', 'Hide'),
+        ],
+    )
