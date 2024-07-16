edd70c23fcc711995a756b5c4dbdd1d70417d9bf
Roger <roger.sans@sygel.es>
2024-07-16 15:57:10 +0200

    [IMP] hide_invoice_shipping_address: pre-commit auto fixes

--- hide_invoice_shipping_address/__manifest__.py
+++ hide_invoice_shipping_address/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hide Invoice Shipping Address',
-    'version': '14.0.1.0.0',
-    'author': 'Sygel, Odoo Community Association (OCA)',
-    'category': 'Reporting',
-    'summary': 'Keep the shipping address hidden on the invoice.',
-    'website': 'https://www.sygel.es',
-    'depends': [
-        'account',
+    "name": "Hide Invoice Shipping Address",
+    "version": "14.0.1.0.0",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "category": "Reporting",
+    "summary": "Keep the shipping address hidden on the invoice.",
+    "website": "https://github.com/sygel-technology/sy-account-financial-reporting",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/res_partner_view.xml',
-        'views/account_move_view.xml',
-        'views/res_config_settings_view.xml',
-        'views/res_partner_view.xml',
-        'report/report_invoice.xml',
+    "data": [
+        "views/res_partner_view.xml",
+        "views/account_move_view.xml",
+        "views/res_config_settings_view.xml",
+        "views/res_partner_view.xml",
+        "report/report_invoice.xml",
     ],
 }
--- hide_invoice_shipping_address/models/res_company.py
+++ hide_invoice_shipping_address/models/res_company.py
@@ -1,13 +1,13 @@
 # Copyright 2024 Roger Sans <roger.sans@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     invoice_shipping_address_print = fields.Boolean(
-        string='Print The Shipping Invoice On The Invoice',
+        string="Print The Shipping Invoice On The Invoice",
         default=True,
     )
