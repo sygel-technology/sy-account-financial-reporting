f7960bb11cb1495e2e2d09184f7789f025ea69c8
Roger <roger.sans@sygel.es>
2024-07-16 11:59:16 +0200

    [MIG] account_financial_report_block_pdf: Migration to 13.0

--- account_financial_report_block_pdf/__manifest__.py
+++ account_financial_report_block_pdf/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Financial Reports Block PDF",
-    "version": "15.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Reporting",
     "summary": "Block the generation of financial PDF reports",
     "author": "Sygel," "Odoo Community Association (OCA)",
