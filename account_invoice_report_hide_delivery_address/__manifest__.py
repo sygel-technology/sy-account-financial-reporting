0939910139dfa180962fdda0021d7320f893784e
Angel Rivas <angel.rivas@sygel.es>
2025-08-01 13:52:25 +0200

    [T-8768][MIG] account_invoice_report_hide_delivery_address: migration to 18.0

--- account_invoice_report_hide_delivery_address/__manifest__.py
+++ account_invoice_report_hide_delivery_address/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Hide Invoice Shipping Address",
-    "version": "15.0.1.0.0",
+    "name": "Account Invoice Report Hide Delivery Address",
+    "version": "18.0.1.0.0",
     "author": "Sygel, Odoo Community Association (OCA)",
     "category": "Reporting",
     "license": "AGPL-3",
@@ -17,7 +17,6 @@
         "views/res_partner_view.xml",
         "views/account_move_view.xml",
         "views/res_config_settings_view.xml",
-        "views/res_partner_view.xml",
         "report/report_invoice.xml",
     ],
 }
